ef326e1c5df7bad62a6018149cc1fa5e5bd29646
leelin <lyt-42@outlook.com>
2023-04-01 16:33:17 +0800

    doc: show list of funcitons

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MyScripts
 py scripts for saving time
+
+
+## Functions list
+
+* sync github repository from local to remote offline server with ssh
--- remote_sync/main.py
+++ remote_sync/main.py
@@ -3,12 +3,15 @@ import os
 cache_path = '/tmp/myScriptCache'
 tmp_repo_path = 'foo-bar'
 
+
 def checkExists(path: str):
     if not os.path.exists(path):
         os.makedirs(path)
-        
+
+
 def getUniHashKey(path: str) -> str:
     return str(hash(path))
+
 
 def cloneRepoFromGithub(path: str, repo: str):
     checkExists(cache_path)
@@ -21,18 +24,26 @@ def cloneRepoFromGithub(path: str, repo: str):
 def args():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--remotePath', help='Path to the folder on remote server', required=True)
+    parser.add_argument('-p',
+                        '--remotePath',
+                        help='Path to the folder on remote server',
+                        required=True)
     parser.add_argument('-r', '--repo', help='Repository URL', required=True)
     parser.add_argument('-s', '--ssh', help='ssh user', required=True)
     parser.add_argument('-P', '--port', help='ssh port', required=True)
     return parser.parse_args()
 
+
 if __name__ == '__main__':
     runtime_arg = args()
     tmp_dir = getUniHashKey(runtime_arg.remotePath)
     cloneRepoFromGithub(tmp_dir, runtime_arg.repo)
-    
+
     # use rsync to sync the repo to remote with ssh
-    print(f'rsync -rv -e "ssh -p {runtime_arg.port}" {os.path.join(cache_path, tmp_dir, tmp_repo_path)} {runtime_arg.ssh}:{runtime_arg.remotePath}')
+    print(
+        f'rsync -rv -e "ssh -p {runtime_arg.port}" {os.path.join(cache_path, tmp_dir, tmp_repo_path)} {runtime_arg.ssh}:{runtime_arg.remotePath}'
+    )
     input('== continue? ==')
-    os.system(f'rsync -rv -e "ssh -p {runtime_arg.port}" {os.path.join(cache_path, tmp_dir, tmp_repo_path)} {runtime_arg.ssh}:{runtime_arg.remotePath}')
+    os.system(
+        f'rsync -rv -e "ssh -p {runtime_arg.port}" {os.path.join(cache_path, tmp_dir, tmp_repo_path)} {runtime_arg.ssh}:{runtime_arg.remotePath}'
+    )
